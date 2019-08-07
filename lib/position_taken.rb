9c74ceca8ce79958c153000fc438cb4f7d839bd1
fred.burgess <fred.burgess@whitehat.org.uk>
2019-08-07 15:13:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,8 @@
 
 def position_taken?(board, index)
   case board[index]
-    when "X" then return true
-    when "O" then return false
+    when "X" then return true;
+    when "O" then return false;
   end
   false
 end
